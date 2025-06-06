4a4ce65fb5aa74e1a3dfc9b4598f2b32cf12b29f
OBSERVA-0 <junioratongi12345@gmail.com>
2025-06-06 02:39:53 -0400

    config changed

--- public/js/config.js
+++ public/js/config.js
@@ -3,7 +3,7 @@ const config = {
         API_BASE_URL: 'http://localhost:3000/api'
     },
     production: {
-        API_BASE_URL: 'https://api.aipluseducation.com/api' // Replace with your actual production API domain
+        API_BASE_URL: 'https://ai-plus-education.onrender.com/api'
     }
 };
 
